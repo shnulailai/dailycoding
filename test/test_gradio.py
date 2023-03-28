b900cf9968873bb126f214e1c3fd049ad9281910
lailai <laisx3@chinaunicom.cn>
2023-03-28 13:15:28 +0800

    Initial commit

---
+++ test/test_gradio.py
@@ -0,0 +1,10 @@
+# gradio搭建演示系统
+
+# 导入gradio
+import gradio as gr
+# 导入transformers
+# from transformers import *
+#
+# gr.Interface.from_pipeline(pipline("text-classification", model="uer/roberta-base-finetuned-dianping-chinese")).lauch()
+
+
